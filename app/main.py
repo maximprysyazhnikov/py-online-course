b1fc085cab35928a9a410a2be4907310354feb2d
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2024-12-24 00:44:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,29 +7,11 @@ class OnlineCourse:
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
-        """
-        Convert days to weeks.
-
-        Args:
-            days (int): The number of days.
-
-        Returns:
-            int: The number of weeks (round up for any extra days).
-        """
-        return (days + 6) // 7  # Round up to the next week if there are extra days.
+        return (days + 6) // 7
 
     @classmethod
     def from_dict(cls, course_dict: dict) -> "OnlineCourse":
-        """
-        Create an instance of OnlineCourse from a dictionary.
 
-        Args:
-            cls: The class itself.
-            course_dict (dict): A dictionary with course data.
-
-        Returns:
-            OnlineCourse: A new instance of the class.
-        """
         weeks = cls.days_to_weeks(course_dict["days"])
         return cls(
             name=course_dict["name"],
